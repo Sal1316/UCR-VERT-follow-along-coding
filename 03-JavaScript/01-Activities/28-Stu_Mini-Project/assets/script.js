baa80702222095b77c9ca31f7e25aa09159c5bdb
Sal Ceja <salceja45@gmail.com>
2023-07-17 16:21:58 -0700

    module 4 upstream download

--- 03-JavaScript/01-Activities/28-Stu_Mini-Project/assets/script.js
+++ 03-JavaScript/01-Activities/28-Stu_Mini-Project/assets/script.js
@@ -1,23 +1,51 @@
 var wins = 0;
 var losses = 0;
 var ties = 0;
+var messageEl = document.querySelector('#message');
 
 var options = ["R", "P", "S"];
 
-var rpsGame = function () {
+var playGame = function () {
   var userChoice = window.prompt("Enter R, P, or S:"); // prompt saves the values inputed by the user. if press cancel, returns undefined.
 
-  if(!userChoice){
-      console.log("Game Ended.")
-      return; // exits the game if there is not user chice.
+  if (!userChoice) {
+    console.log("Game Ended.");
+    messageEl.textContent = "The Game Was Ended. Have a NIce Day!";
+    return; // exits the game if there is not user chice.
   }
 
   userChoice = userChoice.toUpperCase();
 
-  var computerChoice = options[Math.floor(Math.random()*options.length)];
+  var computerChoice = options[Math.floor(Math.random() * options.length)];
 
-  window.alert("The computer chose: " + computerChoice)
+  window.alert("The computer chose: " + computerChoice);
+
+  if (userChoice === computerChoice) {
+    ties++;
+    window.alert("Its a tie!");
+  } else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You won!");
+  } else {
+      losses++;
+      window.alert("You Lost!")
+  }
+
+  window.alert(`Status: \nWins: ${wins} \nLosses: ${losses} \nTies: ${ties}`)
+
+  var playAgain = window.confirm("Play again?")
+
+  if(playAgain) {
+      playGame();// recursion bc you are calling the function you are in.
+  } 
+
+  messageEl.textContent ="Thank you for playing.."
+
 
 };
 
-rpsGame();
+playGame();
