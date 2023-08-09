70f5e095b1b5d9c60da7813547a56a25236490d5
Sal Ceja <salceja45@gmail.com>
2023-08-08 18:19:44 -0700

    changed some things

--- 06-Server-Side-APIs/02-Challenge/Assets/script.js
+++ 06-Server-Side-APIs/02-Challenge/Assets/script.js
@@ -1,40 +1,44 @@
-
-
+// Elements: 
+var searchBtnEl = $("#searchBtn");
+var cityBtns = $("#cityBtns");
 var apiKey = "2b968bda86e14018b6589f7ec132923f";
 
-
-// latLonApi("San Diego");
-// fetchWeatherForcast('Long Beach');
-
 // Event Handlers:
-var searchBtnEl = $("#searchBtn");
 searchBtnEl.on("click", onSeachBtnClick);
+cityBtns.on("click", onCityBtnClick);
 
-// Main Function:
-
+// Functions:
 function onSeachBtnClick() {
-
-      var cityValue = $("#cityInput").val();
-      console.log("cityValue: ", cityValue);
-      latLonApi(cityValue);
-      fetchWeatherForcast(cityValue);
-
+      var cityName = $("#cityInput").val(); // get the value inputed.
+      // console.log("cityName: ", cityName);
+      getLatAndLong(cityName);
+      // fetchWeatherForcast(cityName);
 }
+function onCityBtnClick(event) { // on city button click, pass the inner text to the fetchWeatherForecast fx.
+      event.preventDefault();
+      var cityName = event.target.innerText;
 
-function latLonApi(city) {
+      console.log("oncityNameBtnClick: ", cityName);
+      // call 
+      getLatAndLong(cityName);
+      // fetchWeatherForcast(cityName); // dont need bc its being replaced with getFiveDayForecast().
+      // renderBottomCards()
+}
+function getLatAndLong(city) { // fx gets the latitude and longitude coordinates for the inputed city.
       var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
       fetch(url)
             .then(function (response) {
                   return response.json(); // need to have the return here so we can use the next .then to get the response data.
             })
             .then(function (data) {
-                  console.log("latitude and longitude Data: ", data);
+                  var lat = data[0].lat;
+                  var lon = data[0].lon;
+                  console.log("latitude: " + lat + " longitude: " + lon);
 
-                  fiveDayForecast(data[0].lat, data[0].lon);
+                  getFiveDayForecast(lat, lon);
             });
 }
-
-function fiveDayForecast(lat, lon) {
+function getFiveDayForecast(lat, lon) {
       var url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
       fetch(url, {
@@ -47,72 +51,103 @@ function fiveDayForecast(lat, lon) {
                   return response.json(); // need to have the return here so we can use the next .then to get the response data.
             })
             .then(function (data) {
-                  console.log("fiveDayForecast", data);
-            });
-}
-
-function fetchWeatherForcast(query) { // fx get the weather forecast json object and passes it to renderWeather fx.
-      var url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=imperial&per_page=5&appid=${apiKey}`;
-      var url2 = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
-      // var url2 = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt={5}&appid=${apiKey}`; // use this one.
-
-      fetch(url, {
-            method: "GET", //GET is the default.*GET, POST, PUT, DELETE, etc.
-            credentials: "same-origin", // include, *same-origin, omit
-            redirect: "follow", // manual, *follow, error
-            units: "imperial",
-      })
-            .then(function (response) {
-                  return response.json(); // need to have the return here so we can use the next .then to get the response data.
-            })
-            .then(function (data) {
-                  console.log("Forecast Data:", data);
+                  console.log("getFiveDayForecast", data);
+                  // var lists = data.list
                   renderWeather(data);
+                  renderBottomCards(data);
             });
 }
+/*
+// function fetchWeatherForcast(query) { // NOT SURE IF WE NEE THIS FX. COMBINE WITH getFiveDayForecast()
+//       var url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=imperial&per_page=5&appid=${apiKey}`;
 
-function renderWeather(forecast) {// assigns the json objects values, to their respective element on the single current weather day.
-      console.log("JSON forecast: ", forecast);
-      var resultsContainer = document.getElementById("currentWeather");
+//       fetch(url, {
+//             method: "GET", //GET is the default.*GET, POST, PUT, DELETE, etc.
+//             credentials: "same-origin", // include, *same-origin, omit
+//             redirect: "follow", // manual, *follow, error
+//             units: "imperial",
+//       })
+//             .then(function (response) {
+//                   return response.json(); // need to have the return here so we can use the next .then to get the response data.
+//             })
+//             .then(function (data) {
+//                   // console.log("Forecast Data:", data);
+//                   renderWeather(data);
+//                   // renderBottomCards(data);
+//             });
+// }
+*/
+function renderWeather(forecast) {// assigns the json objects values, to their respective element on the single current weather day.    
+      var cityName = forecast.city.name;
+      var cityTemp = kelvinToFahrenheit(forecast.list[0].main.temp); // [0] will eventually be replaced with i. 
+      var cityWind = forecast.list[0].wind.speed;
+      var cityHumid = forecast.list[0].main.humidity;
+      console.log("renderWeather: " + cityName + ", " + cityTemp + ", " + cityWind + ", " + cityHumid)
+
+      var resultsContainer = document.getElementById("currentWeather"); // convert to jquery after working
 
       var city = document.createElement("h2"); // js or jquery preffered
-      city.textContent = forecast.name;
+      city.textContent = cityName;
       city.classList.add("cityDate"); // adding cityDate class
       resultsContainer.append(city); // append to html.
 
-      var timeStamp = forecast.dt;
-      console.log("timestamp: ", timeStamp)
+      var timeStamp = forecast.list[0].dt;
       var dateObj = dayjs.unix(timeStamp);
-      var formattedDate = dateObj.format('MM/DD/YYYY, h:mm:ss a');
+      var formattedDate = dateObj.format('MM/DD/YYYY');
       // var titleHeader =document.getElementByClassName('cityDate');
       resultsContainer.append(formattedDate); // add next to h2.
 
       var temp = document.createElement("p");
-      temp.textContent = "Temp: " + forecast.main.temp + " F";
+      temp.textContent = "Temp: " + cityTemp + " ℉";
       resultsContainer.append(temp);
-      
+
       var wind = document.createElement("p");
-      wind.textContent = "Wind: " + forecast.wind.speed + " mph";
+      wind.textContent = "Wind: " + cityWind + " mph";
       resultsContainer.append(wind);
 
       var humidity = document.createElement("p");
-      humidity.textContent = "Humidity: " + forecast.main.humidity + " %";
+      humidity.textContent = "Humidity: " + cityHumid + " %";
       resultsContainer.append(humidity);
+
+}
+function renderBottomCards(forecast) {
+      console.log("renderBottomCards: ", forecast);
+
+      // var dayCardsContainer = $("#dayCards");
+      // var data = forecast.list;  // data = 40 arrays
+      // console.log("Data:", data);// 
+
+      // for (var i = 0; data.length < 5; i += 8) { // loops over the midnight time. every 8th array.
+      //       console.log("entered for loop!!!!!!!!!!");
+      //       var timeStamp = forecast.list[i].dt;
+      //       var dateObj = dayjs.unix(timeStamp);
+      //       var formattedDate = dateObj.format('MM/DD/YYYY');
+
+      //       // create elements.
+      //       var card = $(`<div class="card${i}"></div>`); // js or jquery preffered
+      //       var date = $('<p>' + formattedDate + '</p>');
+      //       var ol = $('<ol><li>Temp: ' + data[i].main.temp + '</li><li>Wind: ' + data[i].wind.speed + '</li><li>Humidity: ' + data[i].main.humidity + '</li></ol>');
+
+      //       card.append(date);
+      //       card.append(ol);
+      //       dayCardsContainer.append(card); // you have to sequence these to have nested elements.
+
+      //       console.log("renderBottomCards-dayCardsContainer: ", dayCardsContainer) // day 1 
+
+      // }
+
 }
 
-// use jquery empty fx.
+function kelvinToFahrenheit(k) {
+      var f = ((k - 273.15) * 1.8) + 32;
+      return Math.ceil(f);
+}
 
-// Helper functions:
-// (fetch function for city on search: - need to call the api from the search button.
 
-// fetch for the city on city button press.
+/* Bugs: 
+- not clearing the data when you select a different city.
+- should probrably have a default city loaded first, then user can change it.
 
-// icon function: - displays icons based on temp || clouds || rain
 
-// click handler for the buttons: - load the data from the api request
 
-// search city weather input box.  - when press search, it searches for the city, name and displays instead of the city buttons.
-
-// saveToLocal fx - saves the data to local
-
-// getFromLocal fx - gets the data from local
+*/
