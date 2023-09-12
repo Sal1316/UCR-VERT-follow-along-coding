68668bba42f1ac6b627fe342d3eb98d71823fde9
Sal Ceja <salceja45@gmail.com>
2023-09-11 19:17:37 -0700

    mod commit

--- 11-Express/01-Activities/01-Ins_Setup/server.js
+++ 11-Express/01-Activities/01-Ins_Setup/server.js
@@ -1,20 +1,23 @@
-// Import Express.js
-const express = require('express');
 
-// Import built-in Node.js package 'path' to resolve path of files that are located on the server
-const path = require('path'); // built in to node.js helps us resolve file paths.
+const express = require('express');// Import Express.js
+const path = require('path'); // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 
-// Initialize an instance of Express.js
-const app = express(); 
+const app = express(); // Initialize an instance of Express.js
 
-// Specify on which port the Express.js server will run
-const PORT = 3001; // 3001 is usually the default
 
-// Static middleware pointing to the public folder
-app.use(express.static('public')); // read files from server. Go ahead and serve files from public folder. havt to give express permission to read files first.
+const PORT = 3001; // Specify on which port the Express.js server will run, 3001 is usually the default
+
+
+app.use(express.static('public'));// Static middleware pointing to the public folder
+ /*  
+  the express.static('public') is a middleware by express.js itself. it serves up static files, 
+  such as HTML, CSS, and JS, Images, and other assets to the client (browser) directly from the specified directory. 
+  eed to give express permission to read files first. How?
+ */
 
 // Create Express.js routes for default '/', '/send' and '/routes' endpoints
 app.get('/', (req, res) => res.send('Navigate to /send or /routes')); // be defaul the '/' is looking for an index.html file.
+// res.send() is used to send a response back to the client. In this case, it sends the string 'Navigate to /send or /routes' as the response.
 
 app.get('/send', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/sendFile.html')) // .join just adds to the path url.
