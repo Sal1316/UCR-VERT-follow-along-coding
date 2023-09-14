a74d28fff9228e832c574567c682020d429e4f42
Sal Ceja <salceja45@gmail.com>
2023-09-14 09:27:31 -0700

    mod 12

--- 11-Express/01-Activities/10-Stu_Static-Assets/Unsolved/server.js
+++ 11-Express/01-Activities/10-Stu_Static-Assets/Unsolved/server.js
@@ -4,12 +4,21 @@ const app = express();
 const PORT = 3001;
 const path = require('path');
 
-// Serve images, css files, js files from the public directory
-// Allows us to reference files with their relative path
-// Example: http://localhost:3000/images/cat.jpg
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
 );
+
+
+/* NOTES:
+- why you serve up files with the static middleware, you are basically giving your browser access to them
+  for example, without using any other code, you can access the files in the public folder through the url
+    path.join(__dirname, '/images/google.png'). the route parameters are what accesses the file if it is there.
+    
+
+
+
+*/ 
